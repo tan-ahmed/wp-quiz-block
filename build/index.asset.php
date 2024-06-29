74e89655a648d26cc43cffa305486dd7d1078364
Tan Ahmed <tannerahmed@gmail.com>
2024-06-29 09:31:37 +0100

    update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fb69c4c2509f73dc1c10');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd60d8a3a59b371d38cae');
