86d00fd10ddc827aa01ec0c5d0cb318eca3900e2
Tan Ahmed <tannerahmed@gmail.com>
2024-06-29 09:31:01 +0100

    uodate author

--- readme.txt
+++ readme.txt
@@ -1,12 +1,12 @@
 === Wp Quiz Block ===
-Contributors:      The WordPress Contributors
+Contributors:      Tanveer Ahmed
 Tags:              block
 Tested up to:      6.1
 Stable tag:        0.1.0
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
-Example block scaffolded with Create Block tool.
+Quiz block
 
 == Description ==
 
--- wp-quiz-block.php
+++ wp-quiz-block.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Wp Quiz Block
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       Custom Quiz block
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Tanveer Ahmed
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wp-quiz-block
